60cb2412ab49ef3cc1d7d3cb1c8c99e96a70f118
mikegdro <miguelgdro@gmail.com>
2023-05-05 14:27:50 +0200

    Componente de register/login reusable

---
+++ src/adapters/components/FormSignIn.tsx
@@ -0,0 +1,164 @@
+// React
+import { useContext, useState } from "react";
+import { Navigate } from "react-router-dom";
+
+// Contexto
+import { AuthContext } from "../../context/AuthContext";
+
+// Hooks propios
+import { useApi } from "../../application/api/useApi";
+import { useAuth } from "../../application/customHooks/useAuth";
+
+// Mensaje de error
+import { ErrorMsg } from "./ErrorMsg";
+
+
+// Interfaces
+import { User } from "../../domain/User.interface";
+
+
+// Fotos o SVGs
+import google from "../../assets/svg/login/google.svg";
+import github from "../../assets/svg/login/github.svg";
+
+
+/**
+ * Componente reutilizable de formulario de login/register, le entra una prop,
+ * que es o "login" o "register", y conforme a esto se realiza lo lógia del 
+ * componente, realizando las comprobaciones necesarias, y mostrando los errores
+ * proveído tanto por la IU como por la API.
+ * @param props 
+ * @returns 
+ */
+export const FormSignIn = ( props: { type: "login" | "register" }) => {
+    
+    // Contexto de la app
+    const { user } = useContext(AuthContext);
+
+    // Redirect si el usuario ya está logueado
+    if( user ) {
+        return <Navigate to="/profile" />
+    }
+    
+    // Hook de la Autenticación
+    const { login } = useAuth();
+
+    // Datos del Formulario
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [valid, setValid] = useState<boolean>(false);
+    const [input, setInput] = useState<string>('input-light mt-6');
+
+    /**
+     * Event handler de login que usa el Hook useAuth que usa useUser para crear un usuario 
+     * en el contexto.
+    */
+    const { data, error, loading, fetchData } = useApi<User>();
+    const handleLogin = async () => {
+        await fetchData("http://localhost:8000/login", { 
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: {
+                email: email,
+                password: password,
+            }
+        });
+
+        if( !error?.error ) {
+            login(data!);
+        }
+    };
+
+    const handleRegister = async () => {
+
+    }
+
+    const validate = (valor: string) => {
+        if( valor != password ) {
+            setValid(false);
+            setInput('input input-bordered input-error w-full max-w-xs mt-6');
+            
+        } else {
+            setValid(true);
+            setInput('input-light mt-6');
+        }
+    }
+
+    const sendForm = (event: any) => {
+        // El form no se debería de enviar
+        if( props.type === "register" && valid ) {
+            handleRegister();
+        } else if( props.type === "login" && email.length > 10 && password.length > 5 ) {
+            handleLogin();
+        }
+    }
+
+    return (
+        <>
+            <article className="login-form flex-auto w-full h-full flex items-center">
+                <div className="flex flex-col justify-center items-center min-w-auto w-5/12 m-auto">
+                    <form>
+                        <input 
+                            type="email" 
+                            minLength={10} 
+                            maxLength={80} 
+                            placeholder="Enter email" 
+                            value={email} 
+                            onChange={event => {
+                                setEmail(event.target.value)
+                            }}
+                            className="input-light mb-6"
+                            required
+                        />
+                        <input 
+                            type="password" 
+                            minLength={5} 
+                            placeholder="Enter password" 
+                            className="input-light" 
+                            value={password}
+                            onChange={ event => {
+                                setPassword(event.target.value)
+                            }} 
+                            required
+                        />
+                        { props.type === "register" && (
+                            <input
+                                type="password" 
+                                minLength={2} 
+                                placeholder="Confirm password" 
+                                className={input}
+                                onChange={ event => {
+                                    validate(event.target.value)
+                                }} 
+                                required
+                            />
+                        )}
+
+                        <p className="my-6 cursor-pointer select-none fs-m">Recovery password</p>
+                        <button type="submit" onClick={sendForm} className="btn btn-primary w-full">{props.type}</button>
+                    </form>
+                    
+                    
+
+                    <div className="flex flex-col w-full border-opacity-50">
+                        <div className="divider">OR</div>
+                    </div>
+
+                    <div className="flex justify-between w-full gap-8">
+                        <button className="btn btn-login-others bg-white flex-1"><img src={google}/></button>
+                        <button className="btn btn-login-others btn-login-github flex-1"><img src={github}/></button>
+                    </div>
+
+                    { error?.error == true && (
+                        <ErrorMsg 
+                            message={error.message}
+                        />
+                    )}
+
+                </div>
+            </article>
+        </>
+    ) 
+}
--- src/adapters/pages/Login.tsx
+++ src/adapters/pages/Login.tsx
@@ -1,119 +1,25 @@
-import React, { useContext, useState } from "react";
-
-import { Link, Navigate } from "react-router-dom";
-
-import log from "../../assets/svg/login/login.svg";
-import triangle from "../../assets/svg/login/triangle.svg";
-import google from "../../assets/svg/login/google.svg";
-import github from "../../assets/svg/login/github.svg";
-
-import { useAuth } from "../../application/customHooks/useAuth";
-import { AuthContext } from "../../context/AuthContext";
-import { useApi } from "../../application/api/useApi";
-import { User } from "../../domain/User.interface";
-import { ErrorMsg } from "../components/ErrorMsg";
+import { AsideSignIn } from "../components/AsideSignIn";
+import { FormSignIn } from "../components/FormSignIn";
 
 const Login = () => {
-    // Contexto de la app
-    const { user } = useContext(AuthContext);
-
-    // Hook de la API ( aún no funciona )
-    const { data, error, loading, fetchData } = useApi<User>();
     
-    // Hook de la Autenticación
-    const { login } = useAuth();
-
-    // Datos del Formulario
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-
-    /**
-     * Event handler de login que usa el Hook useAuth que usa useUser para crear un usuario 
-     * en el contexto.
-     */
-    const handleLogin = async () => {
-        await fetchData("http://localhost:8000/login", { 
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: {
-                email: email,
-                password: password,
-            }
-        });
-
-        console.log(undefined)
-
-        if( !error?.error ) {
-            login(data!);
-        }
-    };
-
-    if( user ) {
-        return <Navigate to="/profile" />
+    const registerInfo = {
+        route: "/register",
+        message: "sign up"
     }
 
     return (
         <>
-            <div className="h-100-vh">
-                <div className="flex items-center flex-col lg:flex-row h-full">
-                    <div className="flex-auto h-full flex flex-col justify-center items-center gap-16 relative">
-                        <img src={log} className="select-none"/>
-                        <div className="login-message">
-                            <h1 className="pb-5 font-extrabold">New Here?</h1>
-                            <p className="pb-5 fs-m">Sign up and discover a great amount of new opportunities!</p>
-                            <Link to="/register" className="btn btn-primary w-full">Sign Up</Link>
-                        </div>
-                        <img src={triangle} className="absolute top-0 -right-5 select-none"/>
-                        <img src={triangle} className="absolute bottom-0 -right-5 rotate-180 select-none"/>
-                    </div>
-                    <div className="login-form flex-auto w-full h-full flex items-center">
-                        <div className="flex flex-col justify-center items-center min-w-auto w-5/12 m-auto">
-                            <input 
-                                type="email" 
-                                minLength={3} 
-                                maxLength={80} 
-                                placeholder="Enter email" 
-                                value={email} 
-                                onChange={event => {
-                                    setEmail(event.target.value)
-                                }}
-                                className="input-light mb-6" required
-                            />
-                            <input 
-                                type="password" 
-                                minLength={2} 
-                                placeholder="Enter password" 
-                                className="input-light" 
-                                value={password}
-                                onChange={ event => {
-                                    setPassword(event.target.value)
-                                }} 
-                                required
-                            />
-                            <p className="my-6 cursor-pointer select-none fs-m">Recovery password</p>
-                            <button onClick={handleLogin} className="btn btn-primary w-full">Login</button>
-
-                            <div className="flex flex-col w-full border-opacity-50">
-                                <div className="divider">OR</div>
-                            </div>
-
-                            <div className="flex justify-between w-full gap-8">
-                                <button className="btn btn-login-others bg-white flex-1"><img src={google}/></button>
-                                <button className="btn btn-login-others btn-login-github flex-1"><img src={github}/></button>
-                            </div>
-
-                            { error?.error == true && (
-                                <ErrorMsg 
-                                    message={error.message}
-                                />
-                            )}
-
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <main className="h-100-vh flex items-center flex-col lg:flex-row lg:mx-4">
+                <AsideSignIn 
+                    title={"New Here?"}
+                    message={"Sign up and discover a great amount of new opportunities!"}
+                    link={registerInfo}
+                />
+                <FormSignIn
+                    type={"login"}
+                />
+            </main>
         </>
     );
 };
