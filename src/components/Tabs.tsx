0a1adf0762128de5085a5ee7a6e043f6f8352558
Pablo Valderas Garcia <pvalgarn@gmail.com>
2023-05-25 20:53:13 +0200

    More Little Things

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -14,37 +14,43 @@ function doAction(action: string, target: string) {
     console.log(action + " " + target);
 }
 
-export function Tabs(props: { tab: string, setTab: Function, icon?: string, title: string, links: Array<Tabs> }) {
+export function Tabs(props: { tab: string, setTab: Function, icon?: string, title: string, links?: Array<Tabs> }) {
 
     return (
-        <div className="h-fit md:px-3 py-0 flex justify-between items-center rounded-xl">
-            <div className="flex flex-col md:flex-row items-start md:items-center gap-4 max-[500px]:gap-2 md:gap-7 h-full">
+        <div className={`h-fit ${props.links ? "md:px-3" : "px-3"} py-0 flex justify-between items-center rounded-xl`}>
+            <div className={`flex flex-col md:flex-row items-start md:items-center gap-4 max-[500px]:gap-2 md:gap-7 h-full`}>
                 {/* Header */}
-                <div className="items-center gap-4 max-[500px]:gap-2 hidden md:flex">
+                <div className={`items-center gap-4 max-[500px]:gap-2 ${props.links ? "hidden md:flex" : "flex"} py-[0.73rem]`}>
                     <i className={props.icon + " text-black dark:text-white"}></i>
                     <p className="leading-none text-black dark:text-white text-base">{props.title}</p>
                 </div>
-                <div className="h-9 w-[1.7px] bg-gray-400 dark:bg-slate-500 hidden md:block"></div>
+                {
+                    props.links &&
+                    <div className="h-9 w-[1.7px] bg-gray-400 dark:bg-slate-500 hidden md:block"></div>
+                }
 
                 {/* Tabs */}
-                <div className="flex gap-2">
-                    { props.links.map((link) =>
-                        <li key={link.url} className="list-none">
-                            { props.tab == link.url ? (
+                {
+                    props.links &&
+                    <div className="flex gap-2">
+                        {props.links?.map((link) =>
+                            <li key={link.url} className="list-none">
+                                {props.tab == link.url ? (
                                     <div onClick={(event: React.MouseEvent<HTMLElement>) => { props.setTab(link.url) }}
                                         className="btn btn-primary flex justify-center items-center !text-black dark:!text-white !bg-white dark:!bg-slate-700 !px-3.5 md:!px-5 !py-3 !max-h-none border-none leading-none h-fit min-h-0">
                                         {link.name}
                                     </div>
-                                ): (
+                                ) : (
                                     <div onClick={(event: React.MouseEvent<HTMLElement>) => { props.setTab(link.url) }}
                                         className="btn btn-primary flex justify-center items-center !text-black dark:!text-white !px-3.5 md:!px-5 !py-3 !max-h-none border-none leading-none h-fit min-h-0 !bg-transparent hover:!bg-gray-300 dark:hover:!bg-slate-600">
                                         {link.name}
                                     </div>
                                 )
-                            }
-                        </li>
-                    )}
-                </div>
+                                }
+                            </li>
+                        )}
+                    </div>
+                }
             </div>
         </div>
     )
